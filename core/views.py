7bbce62d225069aa93f3c35f19ffbec1fbd3820c
taysa-fernandes <taysamirelly34@gmail.com>
2023-04-13 15:27:55 -0300

    criando formulario contato

--- core/views.py
+++ core/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
+from django.contrib import messages
 
+from .forms import ContatoForm
 
 def index(request):
     return render(request, 'index.html')
 
 def contato(request):
-    return render(request, 'contato.html')
+    form = ContatoForm(request.POST or None)
+    if str(request.method)=='POST':
+        if form.is_valid():
+            form.send_mail()
+            messages.success(request,'E-mail enviado com sucesso!')
+            form = ContatoForm()
+        else:
+            messages.error(request,'Erro ao enviar e-mail!')
+    context = {
+        'form': form
+    }
+    return render(request, 'contato.html',context)
 
 def produto(request):
     return render(request,'produto.html')
--- django2/settings.py
+++ django2/settings.py
@@ -136,3 +136,16 @@ STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#configurações de e-mail
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+"""
+caso haja o servidor de email no host
+
+EMAIL_HOST = 'localhost'
+EMAIL_HOST_USER = 'no-reply@seudominio.com.br'
+EMAIL_PORT = 587
+EMAIL_USER_TSL = True
+EMAIL_HOST_PASSWORD = 'sua-senha'
+"""
